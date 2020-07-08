b1f8c640ac70fce325f005d6f632fd0420c33682
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-08 06:14:31 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,9 @@
 def unsafe?(speed)
-  speed > 60 ?  "answer" : "answer"
+  speed > 60 ?  "returns true if speed is greater than 60": "returns false if the speed is between 40 and 60"
 end
 
 
 
 def not_safe?(speed)
-  
+
 end
